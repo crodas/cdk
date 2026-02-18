588edcfd4945a07ddb8720db4f127412ddc890c3
lescuer97 <leoescuer@protonmail.com>
2026-02-18 17:05:46 +0100

    add extension

--- crates/cdk-npubcash/src/auth.rs
+++ crates/cdk-npubcash/src/auth.rs
@@ -6,6 +6,7 @@ use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 
 use base64::Engine;
+use cdk_http_client::RequestBuilderExt;
 use nostr_sdk::{EventBuilder, Keys, Kind, Tag};
 use tokio::sync::RwLock;
 
